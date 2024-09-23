821a810f9b05d1fc559b2574b2cc55f9c6e8b6f6
Alejandra Rojas <acrojaslopez@gmail.com>
2024-09-22 23:38:19 -0300

    feat(routes): add market price routes

---
+++ functions/routes/market-prices.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const {db} = require("../config/admin-firestore");
+
+// eslint-disable-next-line new-cap
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const marketPricesRef = db.collection("market-prices");
+  const marketPricesSnapshot = await marketPricesRef.get();
+  const marketPrices = [];
+
+  marketPricesSnapshot.forEach((doc) => {
+    marketPrices.push({
+      id: doc.id,
+      ...doc.data(),
+    });
+  });
+
+  return res.status(201).json({
+    marketPrices,
+  });
+});
+
+router.get("/:market_id", async (req, res) => {
+  const marketPriceRef = db.collection("market-prices")
+      .doc(req.params.market_id);
+
+  const marketPriceDoc = await marketPriceRef.get();
+
+  if (!marketPriceDoc.exists) {
+    return res.status(404).json({message: "the resource has not been found"});
+  }
+
+  const marketPrice = marketPriceDoc.data();
+
+  return res.status(201).json({
+    ...marketPrice,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const {
+    marketId,
+    oneDayVariation,
+    currentPrice,
+  } = req.body.marketPrice;
+
+  const marketPriceRef = db.collection("markets").doc(marketId)
+      .collection("prices");
+
+  const marketPriceDoc = await marketPriceRef.get();
+
+  if (!marketPriceDoc.exists) {
+    await marketPriceRef.set({
+      marketId,
+      oneDayVariation,
+      currentPrice,
+    });
+    const newMarketPriceDoc = await marketPriceRef.get();
+    const newMarketPrice = newMarketPriceDoc.data();
+    return res.status(201).json(newMarketPrice);
+  }
+
+  return res.status(409).json({message: "the resource already exists"});
+});
+
+
+module.exports = router;
